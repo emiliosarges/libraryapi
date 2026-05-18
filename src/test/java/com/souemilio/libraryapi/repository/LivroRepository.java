a0789123548f9b0508d29ebbed0f9c1417fdf75a
emiliosarges <emiliosarges@gmail.com>
2026-05-17 22:04:13 -0300

    aprendendo sobre fetch

--- src/test/java/com/souemilio/libraryapi/repository/LivroRepository.java
+++ src/test/java/com/souemilio/libraryapi/repository/LivroRepository.java
@@ -4,6 +4,7 @@ import com.souemilio.libraryapi.model.Autor;
 import com.souemilio.libraryapi.model.GeneroLivro;
 import com.souemilio.libraryapi.model.Livro;
 import com.souemilio.libraryapi.projection.LivroAutorProjection;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,29 +29,30 @@ class LivroRepositoryTest {
         livro.setPreco(BigDecimal.valueOf(100));
         livro.setGenero(GeneroLivro.FICCAO);
         livro.setTitulo("Outro Livro");
-        livro.setDataPublicacao(LocalDate.of(1980,1,2));
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
 
         Autor autor = autorRepository
                 .findById(UUID.fromString("16e6bde5-0002-4eb4-a8bc-0935f9123ef8"))
-                        .orElse(null);
+                .orElse(null);
 
         livro.setAutor(new Autor());
         livroRepository.save(livro);
     }
 
-    @Test //Aqui funciona sem o cascade
+    @Test
+        //Aqui funciona sem o cascade
     void salvarAutorELivroTest() {
         Livro livro = new Livro();
         livro.setIsbn("456");
         livro.setPreco(BigDecimal.valueOf(200));
         livro.setGenero(GeneroLivro.CIENCIA);
         livro.setTitulo("Terceiro Livro");
-        livro.setDataPublicacao(LocalDate.of(2000,5,17));
+        livro.setDataPublicacao(LocalDate.of(2000, 5, 17));
 
         Autor autor = new Autor();
         autor.setNome("Rodolfo");
         autor.setNacionalidade("Indiano");
-        autor.setDataNascimento(LocalDate.of(1888,1,20));
+        autor.setDataNascimento(LocalDate.of(1888, 1, 20));
 
         autorRepository.save(autor);
 
@@ -66,12 +68,12 @@ class LivroRepositoryTest {
         livro.setPreco(BigDecimal.valueOf(200));
         livro.setGenero(GeneroLivro.CIENCIA);
         livro.setTitulo("Outro Livro");
-        livro.setDataPublicacao(LocalDate.of(2000,5,17));
+        livro.setDataPublicacao(LocalDate.of(2000, 5, 17));
 
         Autor autor = new Autor();
         autor.setNome("João");
         autor.setNacionalidade("Brasileira");
-        autor.setDataNascimento(LocalDate.of(1888,1,20));
+        autor.setDataNascimento(LocalDate.of(1888, 1, 20));
 
         livro.setAutor(autor);
 
@@ -89,5 +91,47 @@ class LivroRepositoryTest {
                         item.getNome() + " - " + item.getTitulo()
                 )
         );
+    }
+
+    @Test
+    void atualizarAutorDoLivroTest() {
+        var idLivro = UUID.fromString("19333e97-1959-45a4-b272-d421b62b3c2e");
+        var livroParaAtualizar = livroRepository
+                .findById(idLivro)
+                .orElse(null);
+
+        var idAutor = UUID.fromString("be34de2c-ffd2-43db-9511-698a5d38cad5");
+        var autorParaAtualizar = autorRepository
+                .findById(idAutor)
+                .orElse(null);
+
+        if (livroParaAtualizar != null) {
+            livroParaAtualizar.setAutor(autorParaAtualizar);
+        }
+
+        livroRepository.save(livroParaAtualizar);
+    }
+
+    @Test
+    void deletarTest() {
+
+    var idLivro = UUID.fromString("46fe76f1-0b7d-4779-b871-95897e910fbe");
+    var livroParaAtualizar = livroRepository
+            .findById(idLivro)
+            .orElse(null);
+    livroRepository.delete(livroParaAtualizar);
+    }
+
+    @Test
+    @Transactional  //LAZY - Busca os dados do autor caso nescessário.
+    void buscarLivroTest() {
+        UUID idLivro = UUID.fromString("19333e97-1959-45a4-b272-d421b62b3c2e");
+        Livro livro = livroRepository.findById(idLivro).orElse(null);
+
+        System.out.println("LIVRO");
+        System.out.println(livro.getTitulo());
+
+        System.out.println("AUTOR");
+        System.out.println(livro.getAutor().getNome());
     }
 }
