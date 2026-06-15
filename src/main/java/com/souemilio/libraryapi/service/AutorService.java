3a0842e88ba537178387c2bfa9f9f9b94ee4096a
emiliosarges <emiliosarges@gmail.com>
2026-06-14 23:56:48 -0300

    Detalhe sobre anotação lombok

--- src/main/java/com/souemilio/libraryapi/service/AutorService.java
+++ src/main/java/com/souemilio/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import com.souemilio.libraryapi.model.Autor;
 import com.souemilio.libraryapi.repository.AutorRepository;
 import com.souemilio.libraryapi.repository.LivroRepository;
 import com.souemilio.libraryapi.validator.AutorValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +13,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AutorService {
 
     private final AutorRepository autorRepository;
     private final AutorValidator validator;
     private final LivroRepository livroRepository;
-
-    public AutorService(AutorRepository autorRepository, AutorValidator validator, LivroRepository livroRepository) {
-        this.autorRepository = autorRepository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
+    //Quando uso @AllArgsConstructor do Lombok, posso eliminar o construtor
+    //Só para nivel de conhecimento, pois no projeto atual que trabalho
+    //não usamos lombok, mas sei usar caso preciso.
+//    public AutorService(AutorRepository autorRepository, AutorValidator validator, LivroRepository livroRepository) {
+//        this.autorRepository = autorRepository;
+//        this.validator = validator;
+//        this.livroRepository = livroRepository;
+//    }
 
     public Autor salvar(Autor autor) {
         validator.validar(autor);
