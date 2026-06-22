908928ea2d7b40c73db79194ba588bef7d5ec03d
emiliosarges <emiliosarges@gmail.com>
2026-06-21 23:38:14 -0300

    Atualizações em DTO e em Controller de Livro

--- src/main/java/com/souemilio/libraryapi/controller/LivroController.java
+++ src/main/java/com/souemilio/libraryapi/controller/LivroController.java
@@ -1,12 +1,15 @@
 package com.souemilio.libraryapi.controller;
 
+import com.souemilio.libraryapi.controller.dto.CadastroLivroDTO;
+import com.souemilio.libraryapi.controller.dto.ErroResposta;
+import com.souemilio.libraryapi.exceptions.RegistroDuplicadoException;
 import com.souemilio.libraryapi.repository.LivroRepository;
 import com.souemilio.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/livros")
@@ -14,5 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
+        try {
+            //mapear dto para entidade
+            //enviar entidade pare o service validar e salvar na base
+            //criar url para acesso dos dados do livro
+            //retornar a respostar codigo created com header location
+
+            return ResponseEntity.ok(dto);
+        } catch (RegistroDuplicadoException e) {
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 
 }
